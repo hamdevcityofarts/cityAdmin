3900241a11d4fc78da6a6773e17c2a70d343ac2b
hamdevcityofarts <hamnsanf@gmail.com>
2026-05-20 18:42:56 +0200

    heigth logo

--- src/dashboard/layout/Sidebar.jsx
+++ src/dashboard/layout/Sidebar.jsx
@@ -154,7 +154,7 @@ const Sidebar = () => {
               <img 
                 src={logo}
                 alt="Grand Hotel Logo" 
-                className="h-10 w-10 object-contain"
+                className="h-14 w-14 object-contain"
               />
             </div>
           </div>
@@ -181,7 +181,7 @@ const Sidebar = () => {
             <img 
               src={logo}
               alt="Grand Hotel Logo" 
-              className="h-10 w-10 object-contain"
+              className="h-14 w-14 object-contain"
             />
           </div>
         </div>
